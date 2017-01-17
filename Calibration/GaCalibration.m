f3a24b23d5987d8ad1fb96572ed7ba036acb7fa5
HJBJalaly <jalaly.hamed@gmail.com>
2017-01-17 19:41:46 +0330

    951028: Calibration, GA

---
+++ Calibration/GaCalibration.m
@@ -0,0 +1,12 @@
+function [x,fval,exitflag,output,population,score] = GaCalibration(lb,ub,PopulationSize_Data,DataLinkagePos)
+%% This is an auto generated MATLAB file from Optimization Tool.
+
+%% Start with the default options
+options = gaoptimset;
+%% Modify options setting
+options = gaoptimset(options,'PopInitRange', [lb,ub]');
+options = gaoptimset(options,'PopulationSize', PopulationSize_Data);
+options = gaoptimset(options,'Display', 'off');
+options = gaoptimset(options,'PlotFcns', {  @gaplotbestf @gaplotbestindiv });
+[x,fval,exitflag,output,population,score] = ...
+ga(@(x)CostError(x,DataLinkagePos),7,[],[],[],[],lb,ub,[],[],options);
